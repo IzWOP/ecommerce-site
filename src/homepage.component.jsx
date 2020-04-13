7c2c4da2f39deda791f68e6588e3ea844b5a1f20
Isaac Vazquez <izzyinjurious@aim.com>
2020-04-12 18:27:32 -0700

    setting up menu

---
+++ src/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className="menu-item">
+                <div className='content'>
+                    <h3 className='title'>Hats</h3>
+                    <p className='subtitle'>Shop Now</p>
+                </div>
+            </div>
+        </div>
+    </div>
+)
