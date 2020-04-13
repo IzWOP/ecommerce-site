510e69044dde8042d122c22ef715b01ce375f921
Izzy <izzyinjurious@aim.com>
2020-04-12 19:32:45 -0700

    added menu styling

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './homepage.styles.scss';
 
 const HomePage = () => (
     <div className='homepage'>
@@ -9,6 +10,32 @@ const HomePage = () => (
                     <p className='subtitle'>Shop Now</p>
                 </div>
             </div>
+            <div className="menu-item">
+                <div className='content'>
+                    <h3 className='title'>Jackets</h3>
+                    <p className='subtitle'>Shop Now</p>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className='content'>
+                    <h3 className='title'>Sneakers</h3>
+                    <p className='subtitle'>Shop Now</p>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className='content'>
+                    <h3 className='title'>Mens</h3>
+                    <p className='subtitle'>Shop Now</p>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className='content'>
+                    <h3 className='title'>Womens</h3>
+                    <p className='subtitle'>Shop Now</p>
+                </div>
+            </div>
         </div>
     </div>
 )
+
+export default HomePage;
